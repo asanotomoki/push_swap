89d9fde7aa6cdcd42e5808faa7083bae88a71d3d
浅野智貴 <mirai20190820@gmail.com>
2022-09-24 06:46:11 +0900

    rule pa, pb

--- srcs/ft_push.c
+++ srcs/ft_push.c
@@ -6,7 +6,44 @@
 /*   By: asanotomoki <asanotomoki@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/24 06:44:52 by asanotomoki       #+#    #+#             */
-/*   Updated: 2022/09/24 06:44:52 by asanotomoki      ###   ########.fr       */
+/*   Updated: 2022/09/24 06:46:04 by asanotomoki      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+void ft_push_front(t_swap *stack, t_swap *new)
+{
+	t_swap *first_elem;
+
+	if (new == NULL)
+		return;
+	if (stack == NULL)
+		init_stack(stack);
+	else
+	{
+		first_elem = ft_first_elem(stack);
+		first_elem->prev->next = new;
+		first_elem->prev = new;
+		new->next = first_elem;
+		new->prev = stack;
+	}
+}
+
+void ft_push_back(t_swap *stack, t_swap *new)
+{
+	t_swap *last_elem;
+	
+	if (new == NULL)
+		return ;
+	if (stack == NULL)
+		init_stack(stack);
+	else
+	{
+		last_elem = ft_last_elem(stack);
+		last_elem->next->prev = new;
+		last_elem->next = new;
+		new->prev = last_elem;
+		new->next = stack;
+	}
+}
--- srcs/stack_operation.c
+++ srcs/stack_operation.c
@@ -6,7 +6,7 @@
 /*   By: asanotomoki <asanotomoki@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 17:12:22 by asanotomoki       #+#    #+#             */
-/*   Updated: 2022/09/24 06:45:03 by asanotomoki      ###   ########.fr       */
+/*   Updated: 2022/09/24 06:45:44 by asanotomoki      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,41 +35,6 @@ t_swap *ft_new_elem(long val)
 	return (elem);
 }
 
-void ft_push_front(t_swap *stack, t_swap *new)
-{
-	t_swap *first_elem;
-
-	if (new == NULL)
-		return;
-	if (stack == NULL)
-		init_stack(stack);
-	else
-	{
-		first_elem = ft_first_elem(stack);
-		first_elem->prev->next = new;
-		first_elem->prev = new;
-		new->next = first_elem;
-		new->prev = stack;
-	}
-}
-
-void ft_push_back(t_swap *stack, t_swap *new)
-{
-	t_swap *last_elem;
-	
-	if (new == NULL)
-		return ;
-	if (stack == NULL)
-		init_stack(stack);
-	else
-	{
-		last_elem = ft_last_elem(stack);
-		last_elem->next->prev = new;
-		last_elem->next = new;
-		new->prev = last_elem;
-		new->next = stack;
-	}
-}
 void init_stack(t_swap *stack)
 {
 	stack->next = stack->prev = stack;
